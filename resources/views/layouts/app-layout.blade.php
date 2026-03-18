cdb212dcd3b9d4ca5c7c99b290ed3d844e44b49b
Sovorthanakk <chh.vorthanak@gmail.com>
2026-03-18 16:47:53 +0700

    Update Name

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -49,7 +49,7 @@
             <span>
                 <div class="sidebar-header">
                     <a href="/">
-                        Test2 System
+                        Test3 System
                     </a>
                     <!-- Add the X button here -->
                     <button id="sidebarToggle" class="sidebar-toggle">
